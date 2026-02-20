d8391bfcec91559f1253596d997978d68863bcc5
farida <farida@gmail.com>
2026-02-20 14:44:59 +0200

    syncing in fastapi with roomdb

--- mobile_backend/app/main.py
+++ mobile_backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-
-from app.routers import task, transaction, users
+from app.routers import task, transaction, users, sync
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(task.router)
 app.include_router(transaction.router)
+app.include_router(sync.router, prefix="/api", tags=["sync"])
