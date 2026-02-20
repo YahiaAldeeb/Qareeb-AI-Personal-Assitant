82ef58f91cfa200e26e8513e07dc4747d8224701
ManarSayedd <manarsayed67@gmail.com>
2026-02-20 16:15:40 +0200

    Removing unnessasry folders(old back with node-Qareeb App(frontend))

--- Qareeb App/app/src/main/java/com/example/qareeb/QareebResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.qareeb
-
-import com.google.gson.annotations.SerializedName
-
-data class QareebResponse(
-    // This tells Gson: "Look for 'transcription' in JSON, but call it 'command' in Kotlin"
-    @SerializedName("transcription")
-    val command: String
-)
