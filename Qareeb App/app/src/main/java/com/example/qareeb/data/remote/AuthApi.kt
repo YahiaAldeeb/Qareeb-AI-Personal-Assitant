a5499bf2fc994e59127452d29fdc0b2e8ddff75c
farida <farida@gmail.com>
2026-02-22 17:32:32 +0200

    updating sgin up

--- Qareeb App/app/src/main/java/com/example/qareeb/data/remote/AuthApi.kt
+++ Qareeb App/app/src/main/java/com/example/qareeb/data/remote/AuthApi.kt
@@ -14,8 +14,23 @@ data class AuthResponse(
     val email: String
 )
 
+data class RegisterRequest(
+    val name: String,
+    val email: String,
+    val password: String,
+    val phoneNumber: String
+)
+
+data class RegisterResponse(
+    val userID: String,
+    val name: String,
+    val email: String
+)
+
 interface AuthApi {
     @POST("users/login")
     suspend fun login(@Body request: LoginRequest): AuthResponse
-}
 
+    @POST("users/register")
+    suspend fun register(@Body request: RegisterRequest): RegisterResponse
+}
