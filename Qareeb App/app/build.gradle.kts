f0922394b830a342fc12a2bdcc1cf34b34f80152
farida <farida@gmail.com>
2026-06-20 23:11:18 +0300

    final fixing

--- Qareeb App/app/build.gradle.kts
+++ Qareeb App/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
     id("org.jetbrains.kotlin.kapt")
+    id("com.google.gms.google-services")
+
 }
 
 android {
